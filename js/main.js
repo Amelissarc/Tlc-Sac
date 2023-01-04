d8c198c6b943d031f62b99b1f7b722c2b94c348b
Amelissarc <melissa.rinconft@gmail.com>
2023-01-04 19:35:45 -0300

    agregando js calculadora

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
+const calculadora = new Calculadora ();
+
+console.log(calculadora.sumar(2,3))
+console.log(calculadora.restar(2,3))
+console.log(Calculadora.dividir(2,3))
+console.log(Calculadora.multiplicar(2,3))

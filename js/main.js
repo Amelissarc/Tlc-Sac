5a8e5ad97cd1184a9cb4934fa4977821f5b52b49
Amelissarc <melissa.rinconft@gmail.com>
2023-01-04 19:27:08 -0300

    agregando js calculadora

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
